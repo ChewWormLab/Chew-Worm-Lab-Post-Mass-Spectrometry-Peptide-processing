fc9d32602a679310e55e89339ffbd189f9e51b80
Ericka Allen <ericka.allen@outlook.com>
2022-11-02 13:38:56 +1030

    Adding coding files - beginning

---
+++ main.py
@@ -0,0 +1,16 @@
+from peptideClass import Peptide  ##this is required to import the class from the code file
+
+##main method
+test_EntireList = Peptide.instantiateFromCSV() ##need to pass the Peptide class as a parameter for the instantiate from CSV method
+##tests of code
+testPeptide = test_EntireList[1]
+queryTest = testPeptide.query
+print(queryTest)
+print(type(test_EntireList[1])) ##should return peptide object = works
+print(len(test_EntireList))
+##testing the biotin sorting method from the list
+
+
+test_BiotinList = Peptide.BiotinList(test_EntireList) #issue with this line - not creating list;maybe issue with code or with how method is passed
+print(len(test_BiotinList))
+
